348b249b7f0df709b08cfb8052ce69c10dd0e9af
Akkuman <akkumans@qq.com>
2020-09-17 12:54:57 +0800

    feat(console): msf console full function implementation

--- homados/apps/duplex/consumers.py
+++ homados/apps/duplex/consumers.py
@@ -4,10 +4,13 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.contrib.auth.models import AnonymousUser
 from libs.msfws import Notify, Console
-from homados.contrib.cache import DistinctCacheProxy
+from homados.contrib.cache import MsfConsoleCache
+from libs.pymetasploit.jsonrpc import MsfJsonRpc, MsfRpcError
 
 
 logger = settings.LOGGER
+
+msfjsonrpc = MsfJsonRpc(server=settings.MSFCONFIG['HOST'], port=settings.MSFCONFIG['JSONRPC']['PORT'], token=settings.MSFCONFIG['JSONRPC']['TOKEN'])
 
 
 class CustomerGroup:
@@ -27,12 +30,9 @@ class BaseCustomer(WebsocketConsumer):
 
 # TODO: 重新启动一个新终端
 class MsfConsoleCustomer(BaseCustomer):
-    CACHE_MSFCONSOLE_INPUT_CACHE = 'msfconsole:input:cache'
-    CACHE_MSFCONSOLE_HISTORY_CACHE = 'msfconsole:history:cache'
-
     def connect(self):
         super().connect()
-        self.cache = DistinctCacheProxy(self.scope['user'].username)
+        self.cache = MsfConsoleCache(self.scope['user'].username)
         self.console = Console(self.channel_name)
 
     def disconnect(self, code):
@@ -41,23 +41,92 @@ class MsfConsoleCustomer(BaseCustomer):
     
     def send_message(self, event):
         message = event['message']
+        if not isinstance(message, str):
+            message = json.dumps(message)
         self.send(message)
+    
+    def pack_msf_output(self, event):
+        message = event['message']
+        message = json.loads(message)
+        logger.debug(message)
+        data: str = message.get('data', '').replace('\n', '\r\n')
+        prompt: str = message.get('prompt', '')
+        data += prompt
+        self.send_input_feedback(data)
 
+    # TODO: 策略模式抽取
     def receive(self, text_data=None, bytes_data=None):
         message = json.loads(text_data)
         input_data = message.get("data")
         
-        cache_input = self.cache.get(self.CACHE_MSFCONSOLE_INPUT_CACHE) or ''
+        cache_input = self.cache.msfconsole_input_cache
         if input_data == '\r' or input_data == '\r\n':
             # 加入命令历史
-            self.cache.msfconsole_history_add(self.CACHE_MSFCONSOLE_HISTORY_CACHE, cache_input)
+            self.cache.msfconsole_history_add(cache_input)
             if cache_input.lower() == 'exit -f':
                 cache_input = 'exit'
             # 发送命令
             self.console.send(cache_input + '\r\n')
             # 清空命令输入
-            self.cache.delete(self.CACHE_MSFCONSOLE_INPUT_CACHE)
-        return super().receive(text_data=self.scope['user'].username)
+            self.cache.msfconsole_input_cache_clear()
+            self.send_input_feedback("\r\n")
+        elif input_data == '\x7f': # backspace 键
+            result = self.cache.msfconsole_input_cache_backspace()
+            self.send_input_feedback(result)
+        elif input_data == '\t': # tab 键
+            tabs = msfjsonrpc.consoles.console(self.console.cid).tabs(cache_input)
+            if not tabs:
+                return
+            elif len(tabs) == 1:
+                extra_str = tabs[0][len(cache_input):]
+                self.send_input_feedback(extra_str)
+                self.cache.msfconsole_input_cache_append(extra_str)
+            else:
+                extra_str = "\r\n"
+                for one in tabs:
+                    extra_str += one + "\r\n"
+                prompt = self.console.prompt
+                extra_str = extra_str + prompt + cache_input
+                self.send_input_feedback(extra_str)
+        elif input_data == "\x1b[A":  # 上键
+            clear_cmd = self.cache.msfconsole_input_cache_clear_online()
+            self.send_input_feedback(clear_cmd)
+            last_cmd = self.cache.msfconsole_history_cache_last()
+            if last_cmd:
+                self.cache.msfconsole_input_cache_append(last_cmd)
+                self.send_input_feedback(last_cmd)
+        elif input_data == "\x1b[B": # 下键
+            clear_cmd = self.cache.msfconsole_input_cache_clear_online()
+            self.send_input_feedback(clear_cmd)
+            next_cmd = self.cache.msfconsole_history_cache_next()
+            if next_cmd:
+                self.cache.msfconsole_input_cache_append(next_cmd)
+                self.send_input_feedback(next_cmd)
+        elif input_data == '\x03':  # ctrl+c
+            msfjsonrpc.consoles.console(self.console.cid).sessionkill()
+            self.cache.msfconsole_input_cache_clear()
+            self.console.send('\r\n')
+            self.send_input_feedback('\r\n')
+        elif input_data == '\x1a':  # ctrl+z
+            msfjsonrpc.consoles.console(self.console.cid).sessiondetach()
+            self.cache.msfconsole_input_cache_clear()
+            self.console.send('\r\n')
+            self.send_input_feedback('\r\n')
+        else:
+            self.cache.msfconsole_input_cache_append(input_data)
+            self.send_input_feedback(input_data)
+
+    def send_input_feedback(self, data=''):
+        message = {}
+        message['status'] = 0
+        message['data'] = data
+        async_to_sync(self.channel_layer.send)(
+            self.channel_name,
+            {
+                'type': 'send_message',
+                'message': message
+            }
+        )
 
 
 class MsfNotifyCustomer(BaseCustomer):
--- homados/homados/contrib/cache.py
+++ homados/homados/contrib/cache.py
@@ -43,10 +43,85 @@ class DistinctCacheProxy:
             return key
         return ':'.join([self.ident_name, key])
 
-    def msfconsole_history_add(self, key, value):
+
+class MsfConsoleCache(DistinctCacheProxy):
+    CACHE_MSFCONSOLE_INPUT_CACHE = 'msfconsole:input:cache'
+    CACHE_MSFCONSOLE_HISTORY_CACHE = 'msfconsole:history:cache'
+    CACHE_MSFCONSOLE_HISTORY_CURSOR = 'msfconsole:history:cursor'
+
+    def msfconsole_history_add(self, value):
         if not value:
             return
-        key = self.get_key(key)
+        key = self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CACHE)
         history = self.cache.get(key) or []
+        self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CURSOR), 0, None)  # 重置光标
         history.insert(0, value)
-        self.cache.set(key, history)
+        self.cache.set(key, history, None)
+
+    @property
+    def msfconsole_history(self):
+        key = self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CACHE)
+        return self.cache.get(key) or []
+
+    @property
+    def msfconsole_input_cache(self):
+        key = self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE)
+        return self.cache.get(key) or ''
+
+    @property
+    def msfconsole_history_cursor(self):
+        key = self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CURSOR)
+        return self.cache.get(key) or 0
+    
+    def msfconsole_input_cache_clear(self):
+        key = self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE)
+        self.cache.delete(key)
+
+    def msfconsole_input_cache_backspace(self):
+        key = self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE)
+        input_cache = self.cache.get(key)
+        if not input_cache:
+            return "\u0007"
+        else:
+            self.cache.set(key, input_cache[0:-1], None)
+            return "\b\u001b[K"
+
+    def msfconsole_input_cache_append(self, data):
+        input_cache = self.msfconsole_input_cache
+        if not input_cache:
+            self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE), data, None)
+            return data
+        else:
+            self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE), input_cache + data, None)
+            return input_cache + data
+
+    def msfconsole_input_cache_clear_online(self):
+        input_cache = self.msfconsole_input_cache
+        if not input_cache:
+            return "\u0007"
+        else:
+            self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_INPUT_CACHE), "", None)
+            return "\b\u001b[K" * len(input_cache)
+
+    def msfconsole_history_cache_last(self):
+        history = self.msfconsole_history
+        if not history:
+            return ''
+        cursor = self.msfconsole_history_cursor
+        self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CURSOR), cursor + 1, None)  # 重置光标
+        cursor = cursor % len(history)
+        return history[cursor]
+
+    def msfconsole_history_cache_next(self):
+        history = self.msfconsole_history
+        if not history:
+            return ''
+        cursor = self.msfconsole_history_cursor
+        if not cursor:
+            self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CURSOR), 0, None)
+            return ''
+        else:
+            self.cache.set(self.get_key(self.CACHE_MSFCONSOLE_HISTORY_CURSOR), cursor - 1, None)  # 重置光标
+        cursor = cursor % len(history)
+        return history[cursor]
+
