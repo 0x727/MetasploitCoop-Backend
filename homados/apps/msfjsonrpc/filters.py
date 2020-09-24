69c2937423ba5c9cc32c94d925500855c7ed55c8
Akkuman <akkumans@qq.com>
2020-09-24 15:26:22 +0800

    fix(filter): fix filter module

--- homados/apps/msfjsonrpc/filters.py
+++ homados/apps/msfjsonrpc/filters.py
@@ -1,13 +1,13 @@
+from django.db.models import fields
 from rest_framework import generics
 from django_filters import rest_framework as filters
 from .models import Modules
 
 
 class ModuleFilter(filters.FilterSet):
+    platform = filters.CharFilter(field_name="platform", lookup_expr='icontains')
+    arch = filters.CharFilter(field_name="arch", lookup_expr='icontains')
+
     class Meta:
         model = Modules
-        fields = {
-            'type': ['exact'],
-            'platform': ['icontains'],
-            'arch': ['icontains'],
-        }
+        fields = ['type', 'platform', 'arch']
