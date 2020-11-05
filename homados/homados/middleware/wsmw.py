e791a25632fee2dc71d61906765e067888cd0b8b
Akkuman <akkumans@qq.com>
2020-11-05 16:27:39 +0800

    fix(middleware): fix auth bug after update channels version

--- homados/homados/middleware/wsmw.py
+++ homados/homados/middleware/wsmw.py
@@ -8,22 +8,21 @@ from channels.auth import get_user
 from urllib.parse import parse_qs
 
 
-# TODO: 跟踪 github.com/django/channels/issues/1399 至文档更新
-class QueryXTokenAuthMiddleware(BaseMiddleware):
-    """时间节点 202009151617
-    一段来自 https://github.com/django/channels/issues/1399 的魔法（django3+channel后出现的问题）
-    
-    直接按照 https://channels.readthedocs.io/en/latest/topics/authentication.html#custom-authentication 写
-    验证中间件会获取不到user"""
-    def populate_scope(self, scope):
+class QueryXTokenAuthMiddleware:
+    def __init__(self, app):
+        # Store the ASGI application we were passed
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        # Look up user from query string (you should also do things like
+        # checking if it is a valid user ID, or if scope["user"] is already
+        # populated).
         self.SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
         params = parse_qs(scope.get('query_string', b'').decode())
         session_key = params.get('token')
         session_key = session_key[0] if session_key else None
         session = self.SessionStore(session_key)
         scope['session'] = session
-        if "user" not in scope:
-            scope["user"] = UserLazyObject()
+        scope['user'] = await get_user(scope)
 
-    async def resolve_scope(self, scope):
-        scope["user"]._wrapped = await get_user(scope)
+        return await self.app(scope, receive, send)
