9292ad62a494237df0a557f5b374478db192aba2
Akkuman <akkumans@qq.com>
2020-11-19 17:31:46 +0800

    feat(restore): auto restore listener

--- homados/apps/msfjsonrpc/apps.py
+++ homados/apps/msfjsonrpc/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 import os
+import time
 
 
 class MsfjsonrpcConfig(AppConfig):
@@ -8,11 +9,13 @@ class MsfjsonrpcConfig(AppConfig):
     def ready(self):
         # 判断 RUN_MAIN 主要原因是因为测试环境保证只运行一次，参见
         # https://stackoverflow.com/questions/33814615/how-to-avoid-appconfig-ready-method-running-twice-in-django/40602348
-        if os.environ.get('RUN_MAIN', None) != 'true':
+        while os.environ.get('RUN_MAIN', None) != 'true':
             try:
                 self._restore_listen()
+                break
             except Exception as e:
                 print('msf jsonrpc service not started')
+                time.sleep(3)
 
     def _restore_listen(self):
         """恢复msf监听器"""
