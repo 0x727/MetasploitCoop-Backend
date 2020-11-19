cab35b4140f1229edcd96834d6776104d9fc4781
Akkuman <akkumans@qq.com>
2020-11-19 18:28:44 +0800

    feat(restore): retry restore listener without lock

--- homados/apps/msfjsonrpc/apps.py
+++ homados/apps/msfjsonrpc/apps.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from django.apps import AppConfig
 import os
 import time
@@ -9,18 +10,20 @@ class MsfjsonrpcConfig(AppConfig):
     def ready(self):
         # 判断 RUN_MAIN 主要原因是因为测试环境保证只运行一次，参见
         # https://stackoverflow.com/questions/33814615/how-to-avoid-appconfig-ready-method-running-twice-in-django/40602348
-        while os.environ.get('RUN_MAIN', None) != 'true':
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            t = Thread(target=self._retry_restore_listen, daemon=True)
+            t.start()
+
+    def _retry_restore_listen(self):
+        """重试恢复msf监听器"""
+        from . import background
+        while True:
             try:
-                self._restore_listen()
+                listeners = background.msfjsonrpc.jobs.list
+                if len(listeners) > 0:
+                    return
+                background.ListenerCacheRecoverThread().start()
                 break
             except Exception as e:
                 print('msf jsonrpc service not started')
-                time.sleep(3)
-
-    def _restore_listen(self):
-        """恢复msf监听器"""
-        from . import background
-        listeners = background.msfjsonrpc.jobs.list
-        if len(listeners) > 0:
-            return
-        background.ListenerCacheRecoverThread().start()
+                time.sleep(5)
